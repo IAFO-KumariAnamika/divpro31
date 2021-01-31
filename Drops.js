a5e4a9c5b926eae781cb0c4efd136229657cb517
IAFO-KumariAnamika <kumarianamika5798@gmail.com>
2021-01-31 17:29:29 +0530

    add

--- Drops.js
+++ Drops.js
@@ -1,23 +1,32 @@
 class Drop{
-    constructor(x,y,r){
+    constructor(x,y){
    var options={
   isStatic:false,
-  restitution:0,
-  friction:1,
-  density:1.2
+  restitution:0.1,
+  friction:0.001,
+  density:0.1
   } 
   this.x=x;
   this.y=y;
-  this.r=r;
+  this.r=5;
  
-  this.body=Bodies.circle(this.x,this.y,this.r/2,options);
+  this.body=Bodies.circle(this.x,this.y,5,options);
   World.add(world,this.body);
  }
+update(){
+
+        if(this.body.position.y>height){
+            Matter.Body.setPosition(this.body,{x:random(0,500),y:random(-50,400)});
+        }
+      
+}
+
  display(){
  push();
+ fill("blue");
  translate(this.body.position.x,this.body.position.y);
- imageMode(CENTER);
- image(this.image,0,0,this.r,this.r);
+ ellipseMode(CENTER);
+ ellipse(0,0,this.r,this.r);
  pop();
  
  
