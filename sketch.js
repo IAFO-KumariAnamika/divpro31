649559c0f728bad15437d19235f871414af8e5ca
IAFO-KumariAnamika <kumarianamika5798@gmail.com>
2021-01-31 17:27:58 +0530

    add

--- sketch.js
+++ sketch.js
@@ -4,21 +4,33 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint=Matter.Constraint;
 
-var drops;
-
+var maxDrops=500;
+var drops=[];
+var rand;
+var thunder, thunder1,thunder2,thunder3,thunder4;
+var umbrellaObj;
+var thunderFrame;
 
 function preload(){
-    
+    thunder1 = loadImage("images/thunderbolt/1.png");
+    thunder2 = loadImage("images/thunderbolt/2.png");
+    thunder3 = loadImage("images/thunderbolt/3.png");
+    thunder4 = loadImage("images/thunderbolt/4.png");
 }
 
 function setup(){
-    createCanvas(1300, 600);
+    createCanvas(500, 470);
 	engine = Engine.create();
 	world = engine.world;
     Engine.run(engine);
 
+    umbrellaObj=new Umbrella(width/2,270);
 
-    drops=new Drops(200,200,10,10);
+    if(frameCount % 100==0){
+    for(var i=0;i<maxDrops;i++){
+        drops.push(new Drop(random(0,500),random(0,450)));
+    }
+}
 
 
 
@@ -28,7 +40,38 @@ function setup(){
 
 function draw(){
     background(0);
+    Engine.update(engine);
 
-    drops.display;
+    for(var i=0;i<maxDrops;i++)
+    {
+        drops[i].display();
+        drops[i].update();
+        
+    }
+
+    rand = Math.round(random(1,4));
+    if(frameCount%80===0){
+        thunderFrame=frameCount;
+        thunder = createSprite(random(0,500), random(10,30), 10, 10);
+        switch(rand){
+            case 1: thunder.addImage(thunder1);
+            break;
+            case 2: thunder.addImage(thunder2);
+            break; 
+            case 3: thunder.addImage(thunder3);
+            break;
+            case 4: thunder.addImage(thunder4);
+            break;
+            default: break;
+        }
+        thunder.scale = random(0.3,0.6);
+    }
+
+    if(thunderFrame + 12 ===frameCount && thunder){
+        thunder.destroy();
+    }
+
+    umbrellaObj.display();
+    drawSprites();
 }   
 
